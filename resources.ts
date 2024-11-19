4a4bdb38f99550af42502e96da8a208943362b6b
Wes Morgan <wes@harperdb.io>
2024-11-19 14:02:56 -0700

    Initial work on the exporter
    Doesn’t fully work yet, but felt like a good place to get a PR review of the initial direction.

---
+++ resources.ts
@@ -0,0 +1,91 @@
+import axios, {AxiosInstance} from 'axios';
+import { server, databases, logger } from 'harperdb';
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from 'url';
+import * as path from "node:path";
+
+interface HydrolixConfig {
+	url: string;
+	username: string;
+	password: string;
+	project: string;
+	table: string;
+}
+
+type HydrolixToken = string;
+
+async function readHydrolixConfig(): Promise<HydrolixConfig> {
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+	const data = await fs.readFile(`${__dirname}/config.json`, {encoding: 'utf-8'});
+	return JSON.parse(data).hydrolix as HydrolixConfig;
+}
+
+function hydrolixClient(baseUrl: string, token: HydrolixToken): AxiosInstance {
+	return axios.create({
+		baseURL: baseUrl,
+		headers: {'Authorization': `Bearer ${token}`},
+	});
+}
+
+async function loginToHydrolix(cfg: HydrolixConfig): Promise<HydrolixToken> {
+	const res = await axios.post(`${cfg.url}/config/v1/login/`,
+		{ username: cfg.username, password: cfg.password });
+	if (res.status === 200) {
+		return res.data.auth_token.access_token;
+	} else {
+		throw new Error(res.statusText);
+	}
+}
+
+type HydrolixClient = AxiosInstance & { hydrolixCfg: HydrolixConfig };
+
+async function initHydrolixSession(cfg: HydrolixConfig): Promise<HydrolixClient> {
+	const token = await loginToHydrolix(cfg);
+	logger.notify("Logged into Hydrolix. Received token:", token);
+	const client = hydrolixClient(cfg.url, token) as HydrolixClient;
+	client.hydrolixCfg = cfg;
+	return client;
+}
+
+async function sendEvent(client: HydrolixClient, event: any) {
+	const cfg = client.hydrolixCfg;
+	// TODO: We may need to defined a "transformer" to teach Hydrolix how to interpret our analytics data
+	return client.post("/ingest/event", event,
+		{ headers: { 'x-hdx-table': `${cfg.project}.${cfg.table}` }}
+	);
+}
+
+async function runExporter() {
+	const cfg = await readHydrolixConfig();
+	logger.notify("Loading Hydrolix config")
+	const hydrolix = await initHydrolixSession(cfg);
+	const events = await databases.system.hdb_raw_analytics.subscribe({
+		omitCurrent: false,
+	});
+	for await (const event of events) {
+		logger.warn("Exporting event to Hydrolix:", event);
+		sendEvent(hydrolix, event).catch(e => logger.error("Error sending event to Hydrolix:", event, e));
+	}
+}
+
+async function run() {
+	if (server.workerIndex === 1) {
+		logger.notify('Running Hydrolix exporter...');
+		await runExporter();
+		logger.notify('Hydrolix exporter shutting down');
+	}
+}
+
+async function generateDummyAnalytics() {
+	if (server.workerIndex === 1) {
+		logger.notify("WHEEEEEEE!!!!!");
+		for (let i = 0; i < 1000; i++) {
+			server.recordAnalytics(i, "loop-de-loop");
+		}
+	}
+}
+
+run();
+
+generateDummyAnalytics();
